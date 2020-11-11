9442d193c188166481b94be3bdfde9149fafb064
Alexander Yukhno <mail@skesh.ru>
2020-11-11 03:07:26 +0300

    upd installation script

--- install.sh
+++ install.sh
@@ -5,58 +5,88 @@ if [ -f "$brew" ]
 then
   echo "Homebrew is installed, nothing to do here"
 else
-  echo "Homebrew is not installed, installing now"
-  echo "This may take a while"
-  echo "Homebrew requires osx command lines tools, please download xcode first"
+  echo "Installing xcode-select & Homebrew"
+  xcode-select --install
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
 fi
 
 brew_packages=(
-"git"
-"python"
-"python3"
-"node"
-"tmux"
-"neovim"
-"kwm"
-"khd"
-"vifm"
-"zsh"
+  "node"
+  "neovim"
+  "vifm"
+  "git"
+  "git-flow"
+  "exa"
+  "imgcat" # for view images in vifm
 )
 
-pip2_packages=(
-	"neovim"
-)
-
-pip3_packages=(
+pip_packages=(
 	"neovim"
 	"dotfiles"
 )
 
-echo "installing brew packages"
-for i in "${brew_packages[@]}"
-do
-  brew install $i
-  echo "---------------------------------------------------------"
+cask_apps(
+  '1password'
+  'alfred'
+  'daisydisk' # buy?
+  'dash' # upgrade?
+  'docker'
+  'google-chrome'
+  'iina'
+  'istat-menus'
+  'iterm2'
+  'karabiner-elements'
+  'little-snitch'
+  'mongodb-compass'
+  'numi' # change!
+  'opera' # change
+  'paw'
+  'skype'
+  'sublime-merge'
+  'sublime-text'
+  'veracrypt'
+  'transmit' # change / forklift
+  'visual-studio-code'
+  'webstorm'
+  'contexts'
+  'sip'
+  'transmission'
+  'postico' # need?
+)
+
+mas_apps=(
+  '747648890'   # Telegram
+  '1438243180'  # Dark Reader for Safari
+  '1449412482'  # Reeder
+  '1176895641'  # Spark
+  '803453959'   # Slack
+  '1480933944'  # Vimari
+  '1320666476'  # Wirp
+  '409203825'   # Numbers
+  '409201541'   # Pages
+  # things
+  # mindnode
+)
+
+echo "Installing Brew packages:"
+for pkg in "${brew_packages[@]}"; do
+  brew install $pkg
 done
 
-echo "installing pip packages"
-for i in "${pip2_packages[@]}"
-do
-	pip2 install $i
-  echo "---------------------------------------------------------"
+echo "Installing Python packages:"
+for pkg in "${pip_packages[@]}"; do
+	pip install $pkg --user
 done
 
-for i in "${pip3_packages[@]}"
-do
-	pip3 install $i
-  echo "---------------------------------------------------------"
+echo "Installing None Store Apps:"
+for app in "${cask_apps[@]}"; do
+	brew cask install $app
 done
 
-echo "installing dotfiles"
-pip install dotfiles
-echo "---------------------------------------------------------"
+echo "Installing Store Apps:"
+for app in "${mas_apps[@]}"; do
+	mas install $app
+done
 
 localGit="/usr/local/bin/git"
 if [ -f "$localGit" ]
@@ -68,13 +98,13 @@ fi
 # Okay so everything should be good
 # Fingers cross at least
 # Now lets clone my dotfiles repo into .dotfiles/
-echo "---------------------------------------------------------"
+# echo "---------------------------------------------------------"
 
-echo "Cloning dotfiles into .dotfiles"
-git clone https://github.com/skesh/dotfiles.git ~/.dotfiles
+# echo "Cloning dotfiles into .dotfiles"
+# git clone https://github.com/skesh/dotfiles.git ~/.dotfiles
 
-cd .dotfiles
-git submodule update --init --recursive
+# cd ~/.dotfiles
+# git submodule update --init --recursive
 
 cd $HOME
 echo "running dotfiles command"
@@ -85,15 +115,7 @@ echo "---------------------------------------------------------"
 ln -s ~/.dotfiles/dotfilesrc ~/.dotfilesrc
 dotfiles -s
 
-echo "---------------------------------------------------------"
-
-#echo "Changing to zsh"
-#chsh -s $(which zsh)
-
-#echo "You'll need to log out for this to take effect"
-#echo "---------------------------------------------------------"
-
-#echo "running oxs defaults"
-#~./osx.sh
+# echo "running oxs defaults"
+# ~./osx.sh
 
 exit 0
